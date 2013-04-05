06b771a88d0040542b63e4d8ea4e1956eb04c462
Sam Nunnery <nunnerys14@mycu.concord.edu>
2013-04-05 15:11:48 -0700

    Create textbased.java

---
+++ textbased.java
@@ -0,0 +1,12 @@
+package textbased;
+
+public class Textbased {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        World map = new World();
+    }
+}
